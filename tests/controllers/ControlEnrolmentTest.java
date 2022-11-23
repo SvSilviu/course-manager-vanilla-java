c15afeb60d4de9f38f5990bd2f846acac5f2dbd6
SvSilviu <silviu.stan18@gmail.com>
2022-11-23 19:18:14 +0200

    resolved

--- tests/controllers/ControlEnrolmentTest.java
+++ tests/controllers/ControlEnrolmentTest.java
@@ -10,10 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ControlEnrolmentTest {
 
     @Test
-    public void testLoadEnrolment(){
+    public void testLoadEnrolment() {
 
         ArrayList<Enrolment> enrolmentArrayList = new ArrayList<>();
-        Enrolment enrolment =new Enrolment(1,2,4);
+        Enrolment enrolment = new Enrolment(1, 2, 4);
 
         ControlEnrolment controlEnrolment = new ControlEnrolment();
         controlEnrolment.loadEnrolment();
@@ -25,7 +25,7 @@ public class ControlEnrolmentTest {
         controlEnrolment.save();
         controlEnrolment.loadEnrolment();
 
-        assertEquals(intialSize+1,controlEnrolment.size());
+        assertEquals(intialSize + 1, controlEnrolment.size());
 
     }
 
@@ -41,6 +41,67 @@ public class ControlEnrolmentTest {
         assertEquals(1, controlEnrolment.celMaiFrecventatCurs());
     }
 
-    
+    @Test
+    public void testListaEnrolmenturilor() {
+
+        ControlEnrolment controlEnrolment = new ControlEnrolment();
+        Enrolment enrolment = new Enrolment(5, 7, 9);
+        Enrolment enrolment1 = new Enrolment(3, 2, 1);
+        Enrolment enrolment2 = new Enrolment(5, 6, 2);
+        Enrolment enrolment3 = new Enrolment(1, 2, 1);
+
+        controlEnrolment.addEnrolment(enrolment);
+        controlEnrolment.addEnrolment(enrolment1);
+        controlEnrolment.addEnrolment(enrolment2);
+        controlEnrolment.addEnrolment(enrolment3);
+
+        ArrayList<Enrolment> enrolmentArrayList = controlEnrolment.listaEnrolmenturilor(5);
+        boolean ver = true;
+
+        for (Enrolment e : enrolmentArrayList) {
+            if (e.getUserID() != 5) {
+                ver = false;
+            }
+        }
+
+        assertEquals(ver, true);
+    }
+
+    @Test
+    public void testVerificareCursuriStudenti() {
+
+        ControlEnrolment controlEnrolment = new ControlEnrolment();
+
+        Enrolment enrolment = new Enrolment(1,2,3);
+        Enrolment enrolment1 = new Enrolment(2,4,3);
+        Enrolment enrolment2 = new Enrolment(5,5,3);
+
+        controlEnrolment.addEnrolment(enrolment);
+        controlEnrolment.addEnrolment(enrolment2);
+        controlEnrolment.addEnrolment(enrolment1);
+
+
+        assertEquals(true,controlEnrolment.verificareCursuriStudent(3,2));
+
+    }
+
+    @Test
+    public void testVerificareCursuriStudentiTrueScenario() {
+
+        ControlEnrolment controlEnrolment = new ControlEnrolment();
+        Enrolment enrolment = new Enrolment(1,2,3000);
+        Enrolment enrolment1 = new Enrolment(2,2,3000);
+        Enrolment enrolment2 = new Enrolment(5,2,3000);
+
+        controlEnrolment.addEnrolment(enrolment);
+        controlEnrolment.addEnrolment(enrolment2);
+        controlEnrolment.addEnrolment(enrolment1);
+
+        boolean verificare = controlEnrolment.verificareCursuriStudent(2,3000);
+
+        controlEnrolment.deleteEnrolment(2,3000);
+
+        assertEquals(verificare, controlEnrolment.verificareCursuriStudent(2, 3000));
+    }
 
 }
